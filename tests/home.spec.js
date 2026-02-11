0d7ca5752e197aea45acf1e46504af6803aabb17
Jhon Marrugo Casa <marrugoarispeyya@gmail.com>
2026-02-10 20:47:49 -0500

    Ingreso a Add new project

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,18 +1,22 @@
 const { test, expect } = require('@playwright/test');
 
-test('Usuario entra al dashboard', async ({ page }) => {
+test('Usuario entra a agregar proyecto', async ({ page }) => {
 
+  test.setTimeout(120000);
   await page.goto('https://clients.onewrapp.com/companies');
 
+  //Ingreso a compañia 525
   await page.waitForSelector('h1');
-
-  await page.waitForTimeout(2000);
-
+  await page.waitForTimeout(3000);
   await page.click('text=525');
+  console.log('Ingreso correcto a proyecto 525');
 
-  await page.waitForTimeout(5000);
+  //Ingreso a agregar nuevo proyecto
+  await page.waitForSelector('h1');
+  await page.getByRole('button', { name: 'Add new project' }).click();
+  await page.waitForTimeout(3000);
+  console.log('Ingreso correcto a Agregar un Proyecto');
 
-  console.log('Ingreso correcto a la compañia 525');
 
 
 });
