2f3551f3d7a68b4d044f15b3d2771671e28a5fc3
Jhon Marrugo <j.marrugo@cincoveinticinco.com>
2026-02-11 16:33:13 -0500

    Cambios para generar reporte correctamente

--- playwright.config.js
+++ playwright.config.js
@@ -5,22 +5,31 @@ module.exports = defineConfig({
 
   testDir: './tests',
 
+  reporter: [
+    ['list'],
+    ['html', { open: 'never' }],
+  ],
+
+  timeout: 120000,
+
+  use: {                    // 👈 MUY IMPORTANTE tener esto global
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure',
+  },
+
   projects: [
     {
       name: 'setup',
       testMatch: /.*\.setup\.spec\.js/,
-      use: {
-        browserName: 'chromium',
-        headless: false,
-      },
     },
     {
       name: 'chromium',
+      dependencies: process.env.CI ? ['setup'] : [],   // 👈 MUY PRO (evita carreras)
       testIgnore: /.*\.setup\.spec\.js/,
       use: {
-        browserName: 'chromium',
         storageState: 'storageState.json',
-        headless: false,
       },
     },
   ],
--- tests/home.spec.js
+++ tests/home.spec.js
@@ -7,14 +7,16 @@ test('Usuario entra a agregar proyecto', async ({ page }) => {
 
   //Ingreso a compañia 525
   await page.waitForSelector('h1');
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1500);
   await page.click('text=525');
   console.log('Ingreso correcto a proyecto 525');
 
   //Ingreso a agregar nuevo proyecto
   await page.waitForSelector('h1');
   await page.getByRole('button', { name: 'Add new project' }).click();
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1500);
   console.log('Ingreso correcto a Agregar un Proyecto');
 
+  await expect (page).toHaveURL('https://clients.onewrapp.com/companies/2/projects')
+  console.log('Obtener URL');
 });
