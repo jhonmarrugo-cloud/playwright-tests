273bf6a777e7e87b99021aeb42d9e8075a7a2264
Jhon Marrugo Casa <marrugoarispeyya@gmail.com>
2026-02-11 21:41:53 -0500

    Hola

--- tests/addProject.spec.js
+++ tests/addProject.spec.js
@@ -34,5 +34,6 @@ test('Agregar un nuevo proyecto', async ({ page }) => {
   // Guardar
   await page.getByRole('button', { name: 'Save' }).click();
   console.log('Se agregó proyecto correctamente');
-  
+
+  console.log('Prueba bloqueo main');
 });
